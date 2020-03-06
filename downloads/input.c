508d8448692553ced5990a79c424f5c786bcecea
griffithcwood <griffithcwood@comcast.net>
2020-03-05 23:17:30 -0500

    Added Download Files

---
+++ downloads/input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  int number;
+  printf("\n Please enter a number: ");
+  scanf("%d", &number);
+  printf("You entered: %d", number);
+  return 0;
+
+}
